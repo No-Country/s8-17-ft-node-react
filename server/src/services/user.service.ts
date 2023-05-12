663a5187201c33ce68e3684cb9748d8f0de2b5b2
ArceDaniel <asd>
2023-05-12 20:14:58 -0300

    change some things

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,10 +1,11 @@
 import Repository from "../utils/repository";
 import { UserModel, User } from "../models/user.model";
+import { UserRegisterDto } from "src/dto/user/userRegister.dto";
 
 export class UserService {
   private userRepository: Repository<User> = new Repository(UserModel);
 
-  public async register(data: any) {
+  public async register(data: UserRegisterDto) {
    return this.userRepository.create(data);
   }
   public async login() {
