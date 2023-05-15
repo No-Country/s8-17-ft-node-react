42c2e2eca9c2da0efc9232cf9fd0e5f0dab80828
arbelais <arbelaism@outlook.com>
2023-05-15 19:09:19 -0300

    feat: remove dark styles. add searchbar component

--- client/src/components/Hero/index.tsx
+++ client/src/components/Hero/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { SearchBar } from "@/components";
 
 type Props = {};
 
 const Hero = (props: Props) => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur
+        cupidatat.
+      </h1>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default Hero;
---
+++ client/src/components/SearchBar/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SearchBar = (props: Props) => {
+  return <input type="text" className="w-2/4 rounded-md shadow-md" placeholder="Search..."></input>;
+};
+
+export default SearchBar;
