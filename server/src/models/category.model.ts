bf1d93c42da4d098a8ff96e9ba39600398c5df4e
Franco <francocorniglione5@gmail.com>
2023-05-17 11:24:06 -0300

    category stuff

--- server/src/models/category.model.ts
+++ server/src/models/category.model.ts
@@ -1,20 +1,28 @@
-import { Prop, getModelForClass } from "@typegoose/typegoose";
+import { getModelForClass, prop } from "@typegoose/typegoose";
+import { IsNotEmpty, IsString } from "class-validator";
 import { v4 as uuidv4 } from "uuid";
 
 export class Category {
-    @Prop({
-        required: true,
-        unique: true,
-        default: uuidv4,
-    })
-    @Prop({
-        required: true,
-    })
-    public name: string;
-    @Prop({
-        required: true,
-    })
-    public description: string; 
+  @prop({
+    required: true,
+    unique: true,
+    default: uuidv4
+  })
+  public id: string;
+
+  @prop({
+    required: true
+  })
+  @IsNotEmpty()
+  @IsString()
+  public name: string;
+
+  @prop({
+    required: true
+  })
+  @IsNotEmpty()
+  @IsString()
+  public description: string;
 }
 
 const CategoryModel = getModelForClass(Category);
