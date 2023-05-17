e829d7198c3d39f01c8c995e1a5b23fdda8a9886
ArceDanielShok <asd>
2023-05-17 14:07:45 -0300

    fix jwt middleware

--- server/src/middleware/jwt.ts
+++ server/src/middleware/jwt.ts
@@ -2,12 +2,15 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 export const checkJWT = (req: Request, res: Response, next: NextFunction) => {
-    const token = <string>req.headers["authorization"];
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        res.locals.jwtPayload = decoded;
-    } catch (error) {
-        return res.status(401).json({ message: "Not authorized" });
-    }
-    next();
-}
+  const authorization = <string>req.headers["authorization"];
+  if (!authorization) return res.status(401).json({ message: "Not authorized" });
+  const token = authorization.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    res.locals.jwtPayload = decoded;
+  } catch (error) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+  next();
+};
