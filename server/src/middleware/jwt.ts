ba966e8039140c36fb4e6b40a7f179077ed06def
Franco <francocorniglione5@gmail.com>
2023-05-17 12:24:11 -0300

    token req.headers refactor

--- server/src/middleware/jwt.ts
+++ server/src/middleware/jwt.ts
@@ -2,12 +2,12 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
 export const checkJWT = (req: Request, res: Response, next: NextFunction) => {
-    const token = <string>req.headers["authorization"];
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        res.locals.jwtPayload = decoded;
-    } catch (error) {
-        return res.status(401).json({ message: "Not authorized" });
-    }
-    next();
-}
+  const [, token] = <string>req.headers["authorization"];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    res.locals.jwtPayload = decoded;
+  } catch (error) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
+  next();
+};
