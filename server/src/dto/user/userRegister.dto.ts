80c62aafa60e65855550a7a103cac5a3c97a1e2e
Franco <francocorniglione5@gmail.com>
2023-05-19 12:59:06 -0300

    dto stuff

--- server/src/dto/user/userRegister.dto.ts
+++ server/src/dto/user/userRegister.dto.ts
@@ -1,8 +1,18 @@
-import { IsNotEmpty, Length } from "class-validator";
-import { UserLoginDto } from "./userLogin.dto";
+import { IsEmail, IsNotEmpty, Length, Matches } from "class-validator";
 
-export class UserRegisterDto extends UserLoginDto {
+export class UserRegisterDto {
   @IsNotEmpty()
   @Length(3, 40)
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+    message:
+      "Password needs an uppercase letter, a lowercase letter, a number and a minimum length of 8 characters"
+  })
+  password: string;
 }
