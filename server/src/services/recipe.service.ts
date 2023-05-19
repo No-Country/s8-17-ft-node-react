edc23d922fa6b2dadb485332d21181068dfa4007
Franco <francocorniglione5@gmail.com>
2023-05-19 18:07:42 -0300

    services stuff

--- server/src/services/recipe.service.ts
+++ server/src/services/recipe.service.ts
@@ -1,10 +1,16 @@
 import Repository from "../utils/repository";
 import RecipeModel, { Recipe } from "../models/recipe.model";
+import UserModel, { User } from "../models/user.model";
 
 export class RecipeService {
   private recipeRepository: Repository<Recipe> = new Repository(RecipeModel);
 
-  // public async save(recipe: any) {
-  //   return this.recipeRepository.create(recipe);
-  // }
+  public async save(data: any): Promise<void> {
+    // lo que se le debe pasar como userId es la propiedad _id del usuario, no su id !!
+    const user: User = await UserModel.findById(data.userId);
+    if (!user) throw new Error("User not found.");
+
+    data.recipe.createdBy = user;
+    await this.recipeRepository.create(data.recipe);
+  }
 }
