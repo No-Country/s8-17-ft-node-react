081cf2b2c5db5363723d59d292e6f6182b084e59
arbelais <arbelaism@outlook.com>
2023-05-19 23:25:45 -0300

    styles: add color variants

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -17,12 +17,45 @@ module.exports = {
         text: ["var(--font-comissioner)"]
       },
       colors: {
-        "primary-americanOrange": "#FF8811",
-        "secondary-brightPink": "#FF0087",
-        "complementary-crayola": "#F4D06F",
-        "text-eerieBlack": "#1E1E1E",
-        "text-darkLiver": "#514B46",
-        "text-taupeGray": "#898989"
+        primary: {
+          50: "#fff3e7",
+          100: "#ffe7cf",
+          200: "#ffcfa0",
+          300: "#ffb870",
+          400: "#ffa041",
+          500: "#ff8811",
+          600: "#cc6d0e",
+          700: "#99520a",
+          800: "#663607",
+          900: "#331b03"
+        },
+        secondary: {
+          50: "#ffe6f3",
+          100: "#ffcce7",
+          200: "#ff99cf",
+          300: "#ff66b7",
+          400: "#ff339f",
+          500: "#ff0087",
+          600: "#cc006c",
+          700: "#990051",
+          800: "#660036",
+          900: "#33001b"
+        },
+        complementary: {
+          50: "#fefaf1",
+          100: "#fdf6e2",
+          200: "#fbecc5",
+          300: "#f8e3a9",
+          400: "#f6d98c",
+          500: "#f4d06f",
+          600: "#c3a659",
+          700: "#927d43",
+          800: "#62532c",
+          900: "#312a16"
+        },
+        "fg-dark": "#1E1E1E",
+        "fg-normal": "#514B46",
+        "fg-light": "#FCFBFB"
       }
     }
   },
