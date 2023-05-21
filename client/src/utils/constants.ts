458fbbc2382f3c9e68f929ca5601250b1a15bd94
arbelais <arbelaism@outlook.com>
2023-05-20 22:01:55 -0300

    styles: add error color

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -4,7 +4,7 @@ export const colors = {
   primary: "#FF8811",
   secondary: "#FF0087",
   complementary: "#F4D06F",
-  error: "#ff0000",
+  error: "#E63946",
   dark: "#1E1E1E",
   normal: "#514B46",
   light: "#898989"
