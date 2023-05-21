2dced420c6617da707f33f5ca6e1928a2c22b811
arbelais <arbelaism@outlook.com>
2023-05-20 22:01:01 -0300

    styles: fix colors

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 import useForm from "@/hooks/useForm";
 import { UserAuth } from "@/types";
 import { loginUser } from "@/backend";
-import Swal from "sweetalert2";
 import { useRouter } from "next/navigation";
 import { useMutation } from "@tanstack/react-query";
+import { alerts } from "@/utils/alert";
+import Cookies from "js-cookie";
+import { USER_TOKEN } from "@/utils/constants";
 
 export default function Login() {
   const router = useRouter();
@@ -16,14 +18,14 @@ export default function Login() {
 
   const { mutate } = useMutation(loginUser, {
     onSuccess: response => {
-      window.localStorage.setItem("loggedUser", JSON.stringify(response.data));
+      // window.localStorage.setItem("loggedUser", JSON.stringify(response.data));
+      Cookies.set(USER_TOKEN, JSON.stringify(response.data), { sameSite: "Lax", expires: 1 });
       router.push("/");
     },
     onError: (error: any) => {
-      Swal.fire({
+      alerts({
         title: error.response.data.message,
-        icon: "error",
-        confirmButtonColor: "#FF8811"
+        icon: "error"
       });
     }
   });
@@ -31,30 +33,28 @@ export default function Login() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (form.email === "" && form.password === "") {
-      Swal.fire({
+      alerts({
         title: "All fields is required!",
-        icon: "warning",
-        confirmButtonColor: "#FF8811"
+        icon: "warning"
       });
     } else if (form.email === "" || form.password === "") {
-      Swal.fire({
+      alerts({
         title: `${form.email === "" ? "Email" : "Password"} is required!`,
-        icon: "warning",
-        confirmButtonColor: "#FF8811"
+        icon: "warning"
       });
     } else {
       mutate(form);
     }
   };
   return (
-    <div className="w-screen h-screen bg-[#fff] flex items-center justify-center">
+    <div className="w-screen h-screen bg-white flex items-center justify-center">
       <main className="w-[800px] h-[600px] flex items-center">
         <section className="h-[520px] w-[40%] flex items-center justify-center">
           <Image src="/CookMeal.png" alt="CookMeal" width={305} height={520} />
         </section>
         <section className="h-[460px] w-[60%] flex flex-col items-center">
-          <h1 className="font-semibold text-4xl text-[#FF8811] gap-18 leading-[57px]">Sign in</h1>
-          <p className="w-[335px] font-light text-[12px] leading-[18px] text-[#514B46] text-center">
+          <h1 className="font-semibold text-4xl text-primary-500 gap-18 leading-[57px]">Sign in</h1>
+          <p className="w-[335px] font-light text-[12px] leading-[18px] text-normal text-center">
             Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de
             tipografías o de borradores de diseño para probar el diseño visual
           </p>
@@ -67,18 +67,18 @@ export default function Login() {
               type="email"
               name="email"
               onChange={handleChange}
-              className="w-[335px] h-[47px] text-[#514B46] shadow-[0px_0px_6px_rgba(0,0,0,0.25)] rounded-[20px] px-[20px] py-[10px] gap-[10px] bg-[#FCFBFB] outline-none"
+              className="w-[335px] h-[47px] text-normal shadow-[0px_0px_6px_rgba(0,0,0,0.25)] rounded-[20px] px-[20px] py-[10px] gap-[10px] bg-white outline-none"
             />
             <input
               placeholder="Password"
               type="password"
               name="password"
               onChange={handleChange}
-              className="w-[335px] h-[47px] text-[#514B46] shadow-[0px_0px_6px_rgba(0,0,0,0.25)] rounded-[20px] px-[20px] py-[10px] gap-[10px] bg-[#FCFBFB] outline-none"
+              className="w-[335px] h-[47px] text-normal shadow-[0px_0px_6px_rgba(0,0,0,0.25)] rounded-[20px] px-[20px] py-[10px] gap-[10px] bg-white outline-none"
             />
             <button
               type="submit"
-              className="w-[335px] h-[47px] rounded-[20px] px-[20px] py-[10px] gap-10 bg-[#FF8811] font-bold text-[16px] leading[27px]"
+              className="w-[335px] h-[47px] rounded-[20px] px-[20px] py-[10px] gap-10 bg-primary-500 font-bold text-[16px]"
             >
               Continue
             </button>
