83f6a9eade54815056036e92c241aa73c22a7da1
arbelais <arbelaism@outlook.com>
2023-05-22 04:37:51 -0300

    styles: register responsive

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -8,7 +8,7 @@ import { useMutation } from "@tanstack/react-query";
 import Cookies from "js-cookie";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import Swal from "sweetalert2";
+import CookMeal from "public/CookMeal.png";
 
 export default function Register() {
   const baseUrl = `${process.env.NEXT_PUBLIC_API_URL}`;
@@ -79,71 +79,56 @@ export default function Register() {
     }
   };
   return (
-    <div className="w-screen h-screen bg-white flex items-center justify-center">
-      <main className="w-[800px] h-[600px] flex items-center">
-        <section className="h-[460px] w-[60%] flex flex-col items-center">
-          <h1 className="font-semibold text-4xl text-primary-500 gap-18 leading-[57px]">
-            Create account
-          </h1>
-          <p className="w-[335px] font-light text-[12px] leading-[18px] text-fg-normal text-center">
-            Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de
-            tipografías o de borradores de diseño para probar el diseño visual Name is email
-            password Continue
-          </p>
-          <form
-            onSubmit={handleSubmit}
-            className="w-[50%] h-[300px] flex flex-col items-center justify-evenly"
-          >
-            <input
-              placeholder="Name is"
-              onChange={handleChange}
-              name="name"
-              value={form.name}
-              type="text"
-              className="w-[335px] h-[47px] text-fg-normal shadow-[0px_0px_6px_rgba(0,0,0,0.25)] rounded-[20px] px-[20px] py-[10px] gap-[10px] bg-[#FCFBFB] outline-none"
-            />
-            <input
-              placeholder="Email"
-              onChange={handleChange}
-              name="email"
-              value={form.email}
-              type="email"
-              className="w-[335px] h-[47px] text-[#514B46] shadow-[0px_0px_6px_rgba(0,0,0,0.25)] rounded-[20px] px-[20px] py-[10px] gap-[10px] bg-[#FCFBFB] outline-none"
-            />
-            <input
-              placeholder="Password"
-              onChange={handleChange}
-              name="password"
-              value={form.password}
-              type="password"
-              className="w-[335px] h-[47px] text-[#514B46] shadow-[0px_0px_6px_rgba(0,0,0,0.25)] rounded-[20px] px-[20px] py-[10px] gap-[10px] bg-[#FCFBFB] outline-none"
-            />
-            <button
-              type="submit"
-              className="w-[335px] h-[47px] rounded-[20px] px-[20px] py-[10px] gap-10 bg-[#FF8811] font-bold text-[16px] leading[27px]"
-            >
-              Continue
-            </button>
-          </form>
-          <div className="w-[150px] flex items-center justify-around">
-            <button
-              onClick={handleRegisterFacebook}
-              className="w-[50px] h-[50px] shadow-[0px_0px_6px_rgba(0,0,0,0.25)] p-[10px] rounded-[8px]"
-            >
-              <Image src="/Facebook.png" width={30} height={30} alt="Facebook" />
-            </button>
-            <button
-              onClick={handleRegisterGoogle}
-              className="w-[50px] h-[50px] shadow-[0px_0px_6px_rgba(0,0,0,0.25)] p-[10px] rounded-[8px]"
-            >
-              <Image src="/Google.png" width={30} height={30} alt="Google" />
-            </button>
-          </div>
-        </section>
-        <section className="h-[520px] w-[40%] flex items-center justify-center">
-          <Image src="/CookMeal.png" alt="CookMeal" width={305} height={520} />
-        </section>
-      </main>
+    <div className="container h-full bg-white flex items-center justify-center m-auto py-16 px-4">
+      <section className="w-full md:w-1/3 flex flex-col items-center justify-center gap-5">
+        <h1 className="text-center font-semibold text-4xl text-primary-500">Create account</h1>
+        <p className="font-light text-xs text-normal text-center">
+          Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de
+          tipografías o de borradores de diseño para probar el diseño visual Name is email password
+          Continue
+        </p>
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col items-center justify-evenly gap-6"
+        >
+          <input
+            placeholder="Name is"
+            onChange={handleChange}
+            name="name"
+            value={form.name}
+            type="text"
+            className="w-full text-normal shadow-[0px_0px_6px_rgba(0,0,0,0.25)] rounded-3xl px-5 py-3 bg-white outline-none"
+          />
+          <input
+            placeholder="Email"
+            onChange={handleChange}
+            name="email"
+            value={form.email}
+            type="email"
+            className="w-full text-normal shadow-[0px_0px_6px_rgba(0,0,0,0.25)] rounded-3xl px-5 py-3 bg-white outline-none"
+          />
+          <input
+            placeholder="Password"
+            onChange={handleChange}
+            name="password"
+            value={form.password}
+            type="password"
+            className="w-full text-normal shadow-[0px_0px_6px_rgba(0,0,0,0.25)] rounded-3xl px-5 py-3 bg-white outline-none"
+          />
+          <button type="submit" className="w-full rounded-3xl px-5 py-3 bg-primary-500 font-bold">
+            Continue
+          </button>
+        </form>
+        <div className="flex items-center justify-around gap-5">
+          <button className="w-[50px] h-[50px] shadow-[0px_0px_6px_rgba(0,0,0,0.25)] p-[10px] rounded-[8px]">
+            <Image src="/Facebook.png" width={30} height={30} alt="Facebook" />
+          </button>
+          <button className="w-[50px] h-[50px] shadow-[0px_0px_6px_rgba(0,0,0,0.25)] p-[10px] rounded-[8px]">
+            <Image src="/Google.png" width={30} height={30} alt="Google" />
+          </button>
+        </div>
+      </section>
+      <Image src={CookMeal} alt="CookMeal" className="hidden md:block" />
     </div>
   );
 }
