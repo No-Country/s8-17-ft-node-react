bcea70163608f3fefbb482640d8edfff0f7ee9c1
Franco <francocorniglione5@gmail.com>
2023-05-23 11:09:36 -0300

    controllers stuff

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,3 +1,4 @@
 export { AuthController } from "./auth.controller";
 export { RecipeController } from "./recipe.controller";
 export { CategoryController } from "./category.controller";
+export { DietController } from "./diet.controller";
