e8133656b7c05b61a979ffeb84526cd4ee4b40bf
Franco <francocorniglione5@gmail.com>
2023-06-01 18:03:01 -0300

    routes stuff

--- server/src/routes/recipe.routes.ts
+++ server/src/routes/recipe.routes.ts
@@ -5,16 +5,21 @@ import { checkJWT } from "../middleware/jwt";
 import { UserService } from "../services/user.service";
 
 const recipeRoutes = Router();
-const recipeController = new RecipeController(
-    new RecipeService(),
-    new UserService(),
-    );
+const recipeController = new RecipeController(new RecipeService(), new UserService());
 
 recipeRoutes.post("/generate", checkJWT, recipeController.generate.bind(recipeController));
 recipeRoutes.post("/search", checkJWT, recipeController.search.bind(recipeController));
-recipeRoutes.post("/add-favorite/:id", checkJWT, recipeController.addFavorite.bind(recipeController));
-recipeRoutes.delete("/delete-favorite/:id", checkJWT, recipeController.deleteFavorite.bind(recipeController));
-recipeRoutes.get("/", recipeController.getAll.bind(recipeController));
+recipeRoutes.post(
+  "/add-favorite/:id",
+  checkJWT,
+  recipeController.addFavorite.bind(recipeController)
+);
+recipeRoutes.delete(
+  "/delete-favorite/:id",
+  checkJWT,
+  recipeController.deleteFavorite.bind(recipeController)
+);
+recipeRoutes.get("/", checkJWT, recipeController.getAll.bind(recipeController));
 recipeRoutes.get("/favorite", checkJWT, recipeController.getFavoriteByUser.bind(recipeController));
 recipeRoutes.get("/createdBy", checkJWT, recipeController.getCreatedBy.bind(recipeController));
 recipeRoutes.get("/id/:id", recipeController.getById.bind(recipeController));
